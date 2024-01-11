38dafbe85f815226bbd49995106bde254dd55f33
drumath2237 <2237k.drumath@gmail.com>
2024-01-11 23:10:53 +0900

    :sparkles: some change

---
+++ MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+public partial class Util
+{
+    [JSImport("util.fun", "main.js")]
+    public static partial int GetInt(int val);
+}
+
+public partial class MyClass
+{
+    [JSExport]
+    public static string Greeting()
+    {
+        var text = $"Hello, World! Greetings from {GetHRef()}";
+        return text;
+    }
+
+    [JSImport("window.location.href", "main.js")]
+    public static partial string GetHRef();
+}
--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,9 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-import { dotnet } from './_framework/dotnet.js'
+import {dotnet} from './_framework/dotnet.js'
 
-const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
+const {setModuleImports, getAssemblyExports, getConfig} = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
@@ -13,11 +13,18 @@ setModuleImports('main.js', {
         location: {
             href: () => globalThis.window.location.href
         }
+    },
+    util: {
+        fun: (val) => val + 1
     }
 });
 
+
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
+
+console.log(exports)
+
 const text = exports.MyClass.Greeting();
 console.log(text);
 
