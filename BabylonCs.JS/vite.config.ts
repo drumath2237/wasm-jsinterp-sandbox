378b8585e8ed82cf6445f959a30b4215676e49e2
drumath <2237k.drumath@gmail.com>
2024-01-13 23:59:29 +0900

    :wrench: vite config を編集

--- BabylonCs.JS/vite.config.ts
+++ BabylonCs.JS/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter'
-    }
-  }
-})
+      entry: resolve(__dirname, "./lib/index.ts"),
+      name: "babyloncs-js",
+      fileName: "index",
+      formats: ["es", "umd"],
+    },
+  },
+});
