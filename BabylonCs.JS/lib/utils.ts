da5684c65847dc590eaa95e09d554ad081318fb0
drumath <2237k.drumath@gmail.com>
2024-01-14 17:53:16 +0900

    :zap: コードベースの整理

---
+++ BabylonCs.JS/lib/utils.ts
@@ -0,0 +1,12 @@
+/**
+ * log message to p element
+ * @param msg log message
+ */
+export const logInfo = (msg: string): void => {
+  const logElm = document.querySelector<HTMLParagraphElement>("p#msg");
+  if (!logElm) {
+    return;
+  }
+
+  logElm.textContent = msg;
+};
